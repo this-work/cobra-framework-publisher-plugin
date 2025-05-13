4cb4e97a790ce35120eaca3d3be249d5787bb98d
Sascha Dücking <sascha.duecking@this.work>
2025-05-13 16:33:58 +0200

    Refactor
    - Switch to using concurrency queue instead of chunks
    - Improve download perf
    - Improve error handling/logging
    - Clean up/add comments
    - Add prettier config

--- README.md
+++ README.md
@@ -1,22 +1,44 @@
 # Cobra-Framework Pubisher Plugin
 Nuxt module for a publish process. Requires a minimal version of nuxt 2.17+
 
-
 ### Requirements
 - Nuxt 2.17+
-
 
 ### Usage in Nuxt
 
 Install plugin dependencies
-``` bash
+
+```bash
 $ npm install @this/cobra-framework-publisher-plugin
 ```
 
-Add module in nuxt.config.js
-``` js
+Add module to `nuxt.config.js`:
+
+```js
 modules: [
-    ['@this/cobra-framework-publisher-plugin'],
+    ['@this/cobra-framework-publisher-plugin']
 ]
 ```
 
+#### Module Options
+
+```js
+modules: [
+    [
+        '@this/cobra-framework-publisher-plugin',
+        {
+            // Directory where downloaded assets will be stored
+            destination: 'dist',
+
+            // Number of assets to download concurrently
+            chunkSize: 250,
+
+            // Name of the Nuxt payload files to scan
+            payloadFileName: 'payload.js',
+
+            // Path where payload files are located
+            payloadFilePath: '/_nuxt/static'
+        }
+    ]
+]
+```
--- src/module.js
+++ src/module.js
@@ -1,74 +1,56 @@
 /**
  * Cobra - Publisher
  *
+ * @module @this/cobra-framework-publisher-plugin
  * @description Nuxt module for a publish process.
- * Requires a minimal version of nuxt 2.17+
  *
- * @version 1.0.0
+ * @requires nuxt ^2.17.0
  * @author Tobias Wöstmann
- *
+ * @version 1.0.0
  */
 
-const fs = require('fs-extra');
-import AssetLoaderClass from './AssetLoader.class';
+import fs from 'fs-extra';
+import AssetLoader from './AssetLoader.class.js';
 
-let publisherAlreadyStarted = false;
-let AssetLoader = false;
+/** @type {import('./AssetLoader.class.js').default|null} */
+let assetLoader = null;
 
-export default async function(moduleOptions) {
-
-    const { nuxt } = this;
-
-    const nuxtConfig = nuxt.options;
-
-    if (nuxtConfig.publicRuntimeConfig.API.length > 0) {
-
-        if (!publisherAlreadyStarted) {
-
-            publisherAlreadyStarted = true;
-
-            AssetLoader = new AssetLoaderClass(
-                nuxtConfig.publicRuntimeConfig.API,
-                moduleOptions.destination,
-                moduleOptions.chunkSize,
-                moduleOptions.payloadFileName,
-                moduleOptions.payloadFilePath,
-            );
-
-        }
-
-        this.nuxt.hook("generate:done", async (generator, errors) => {
-
-            AssetLoader.collect();
-
-            await AssetLoader.download();
-
-            if (errors.length > 0) {
-
-                fs.writeFileSync(
-                    generator.options.generate.dir + '/error.log',
-                    JSON.stringify(
-                        { 'routes': Array.from(generator.generatedRoutes),
-                            'errors': errors.map(error => {
-                                return {
-                                    type: error.type,
-                                    route: error.route,
-                                    error: error.error.toString()
-                                };
-                            }) }
-                    )
-                );
-            } else {
-
-                fs.writeFileSync(
-                    generator.options.generate.dir + '/success.log',
-                    'success'
-                );
-
-            }
-
-        });
-
+/**
+ * @param {Object} moduleOptions - Options passed to the module in nuxt config definition
+ * @param {string} [moduleOptions.destination] - Destination directory for downloaded assets
+ * @param {number} [moduleOptions.chunkSize] - Number of assets to download concurrently
+ * @param {string} [moduleOptions.payloadFileName] - Name of the payload files to scan
+ * @param {string} [moduleOptions.payloadFilePath] - Path where payload files are located
+ * @returns {Promise<void>}
+ */
+export default async function (moduleOptions) {
+    if (!this.nuxt.options.publicRuntimeConfig.API?.length) {
+        consola.warn('No API configured');
+        return;
     }
 
+    if (!assetLoader) {
+        assetLoader = new AssetLoader({
+            host: this.nuxt.options.publicRuntimeConfig.API,
+            destination: moduleOptions.destination,
+            chunkSize: moduleOptions.chunkSize,
+            payloadFileName: moduleOptions.payloadFileName,
+            payloadFilePath: moduleOptions.payloadFilePath
+        });
+    }
+
+    this.nuxt.hook('generate:done', async (generator, errors) => {
+        assetLoader.collect();
+        await assetLoader.download();
+
+        const logPath = `${generator.options.generate.dir}/${errors.length > 0 ? 'error' : 'success'}.log`;
+        const logContent =
+            errors.length > 0
+                ? JSON.stringify({
+                      routes: Array.from(generator.generatedRoutes),
+                      errors: errors.map(({ type, route, error }) => ({ type, route, error: error.toString() }))
+                  })
+                : 'success';
+        fs.writeFileSync(logPath, logContent);
+    });
 }
